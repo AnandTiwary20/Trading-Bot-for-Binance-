6aaa1e849fbd018dbba1d3c942b9880ee1530ec0
AnandTiwary20 <anandtiwari0019@gmail.com>
2026-03-18 14:12:56 +0530

    added API KEYS

---
+++ bot/config.py
@@ -0,0 +1,3 @@
+API_KEY = "8qm4Sifhnn03nwmzFLMTkIHW62hkIZmV9a8y2xW0xEPKli7HHpZKxhHqmJlXEHRg"
+API_SECRET = "kNwvVMNU3ZIrxo1mMUSDBa1SPZsy8s6EO9mmiXPVzZPBPmxU4HaIVdLd8Qy8tPEE"
+BASE_URL = "https://testnet.binancefuture.com"
