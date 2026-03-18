58357d16b878938fa952f50c531d2d63a9cf54ce
AnandTiwary20 <anandtiwari0019@gmail.com>
2026-03-18 14:48:25 +0530

    added core logic in main

--- bot/main.py
+++ bot/main.py
@@ -4,3 +4,17 @@ from orders import place_market_order, place_limit_order
 
 def main():
     setup_logger()
+
+    client = get_client()
+
+    symbol = "BTCUSDT"
+    quantity = 0.001
+
+    place_market_order(client, symbol, "BUY", quantity)
+    place_limit_order(client, symbol, "SELL", quantity, price="70000")
+
+if __name__ == "__main__":
+    main()
+
+# place_market_order(client, symbol, "BUY", quantity)
+# place_limit_order(client, symbol, "SELL", quantity, price="70000")
