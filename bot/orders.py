587f0c81865db665188957ff75435cc9871457e0
AnandTiwary20 <anandtiwari0019@gmail.com>
2026-03-18 14:42:04 +0530

    Upgraded the error boundaries and did mapping

--- bot/orders.py
+++ bot/orders.py
@@ -3,13 +3,18 @@ import logging
 
 def place_market_order(client, symbol, side, quantity):
     try: 
-        order =client.futures_create_order(
+        order =client.futures_create_order(  
+                                                      #may need to failure if the input is not validated
             symbol=symbol,
             side=side,
             type="MARKET",
             quantity=quantity
+                                                #  Mapping internal function inputs  into external API contract
+                                                 #  This is called DTO mapping (Data Transfer Object)
+
         )
         logging.info(f"Market order placed: {order}")
+        
         return order
     except Exception as e:
         logging.error(f"Error placing market order: {e}")
